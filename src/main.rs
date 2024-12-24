ed65f8b8040dd72b8e0dcf12e3661f5c7472b742
rayer456 <electrorayer@gmail.com>
2024-12-24 20:07:51 +0100

    Removed previous, unused GUI elements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{thread::sleep, time::{Duration, SystemTime}};
 
-use eframe::{egui::{self, Color32, Key, Label, Modifiers, Rect, RichText}, Frame};
+use eframe::{egui::{self, Color32, Label, RichText}};
 use tokio::{spawn, sync::mpsc::{self, Receiver}};
 
 mod bthr;
@@ -17,7 +17,7 @@ async fn main() {
 
     let native_options = eframe::NativeOptions::default();
     let _ = eframe::run_native(
-        "My app", 
+        "bthr", 
         native_options, 
         Box::new(|cc| Ok(Box::new(MyApp::new(cc, rx)))),
     );
@@ -26,7 +26,7 @@ async fn main() {
 
 struct MyApp {
     btrx: Receiver<u8>,
-    data: u8,
+    live_heart_rate: u8,
     frame_time: Duration,
 }
 
@@ -34,76 +34,46 @@ impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>, btrx: Receiver<u8>) -> Self {
         MyApp {
             btrx,
-            data: 0,
+            live_heart_rate: 0,
             frame_time: Duration::from_secs_f64(1.0/MAX_FPS),
         }
     }
 }
 
+impl MyApp {
+    fn update_live_heart_rate(&mut self) {
+        self.live_heart_rate = self.btrx.try_recv().unwrap_or(self.live_heart_rate);
+    }
+}
+
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // ctx.request_repaint_after(Duration::from_millis(150));
+        
+        let now = SystemTime::now(); // FPS related
+
+        // Update HR live_heart_rate
+        self.update_live_heart_rate();
 
 
-        let now = SystemTime::now();
+        // GUI
+        let live_hr_text = RichText::new(format!("HR: {}", self.live_heart_rate.to_string()))
+            .color(Color32::RED)
+            .background_color(Color32::WHITE)
+            .size(40.0);
 
-        let range = egui::Rangef::new(200.0, 250.0);
-        let frame = egui::Frame::none().fill(Color32::LIGHT_BLUE);
+        let live_hr_label = Label::new(live_hr_text);
 
-        let top_panel = egui::TopBottomPanel::top("top_panel")
-            .height_range(range)
-            .frame(frame);
-
-        top_panel.show(ctx, |ui| {
-            let r_side_panel = egui::SidePanel::right("right_side_panel")
-                .min_width(50.0)
-                .max_width(120.0);
-
-            r_side_panel.show(ctx, |ui| {
-                ui.label("some text on the top right panel");
-            });
-
-            ui.label("Some text on the left side of the top panel");
-        });
-
-        let bottom_panel = egui::TopBottomPanel::bottom("bottom_panel")
-            .height_range(range);
-
-        /* if ctx.input(|i| i.modifiers.contains(Modifiers::SHIFT) && i.key_down(Key::B)) {
-            self.text_val = "B is being pressed while holding shift".to_string();
-        } else {
-            self.text_val = "default text".to_string();
-        } */
-
-
-        self.data = self.btrx.try_recv().unwrap_or(self.data);
-
-
-        let my_text = RichText::new(self.data.to_string()).color(Color32::RED).background_color(Color32::WHITE);
-        let my_label = Label::new(my_text).halign(egui::Align::Center);
-        bottom_panel.show(ctx, |ui| {
-            
-            ui.add(my_label);
-
-            ui.horizontal(|ui| {
-                ui.label("test hor 1");
-                ui.label("test hor 2");
-            });
-
-
-            let collapsing_header = egui::CollapsingHeader::new("Collapsing header").default_open(true);
-            collapsing_header.show(ui, |ui2| {
-                ui2.label("text in collapsing header");
-            });
+        let central_panel = egui::CentralPanel::default();
+        central_panel.show(ctx, |ui| {
+            ui.add(live_hr_label);
 
 
         });
 
-        let elapsed = now.elapsed().unwrap();
 
+        // FPS related
+        let elapsed = now.elapsed().unwrap_or(Duration::from_micros(300));
         ctx.request_repaint();
         sleep(self.frame_time - elapsed);
-
-        
     }
 }
