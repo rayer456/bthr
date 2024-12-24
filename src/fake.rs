a0e5b5131775cea6bc83f624a5a000a25b170615
rayer456 <electrorayer@gmail.com>
2024-12-25 00:04:07 +0100

    updated fake hr function

--- src/fake.rs
+++ src/fake.rs
@@ -6,11 +6,7 @@ use tokio::sync::mpsc::Sender;
 
 pub async fn transmit_fake_hr_data(tx: Sender<u8>) {
     loop {
-        let random = {
-            let mut rng = rand::thread_rng();
-            rng.gen_range(50..70)
-        };
-        
+        let random = rand::thread_rng().gen_range(50..70);
         let _ = tx.send(random).await;
         tokio::time::sleep(Duration::from_millis(800)).await;
     }
