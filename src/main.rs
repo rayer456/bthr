6ee7aa2cebf9a33d6cd4185cdd55f8771b7e980c
rayer456 <electrorayer@gmail.com>
2024-12-27 23:27:31 +0100

    make the discovered peripherals into a clickable button

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{thread::sleep, time::{Duration, SystemTime}};
 
 use btleplug::platform::Peripheral;
 use btleplug::api::Peripheral as _;
-use eframe::{egui::{self, Color32, Label, RichText}, glow::INFO_LOG_LENGTH};
+use eframe::{egui::{self, Button, Color32, Label, RichText, Rounding}, glow::INFO_LOG_LENGTH};
 use tokio::{spawn, sync::mpsc::{self, Receiver}};
 
 mod bthr;
@@ -58,17 +58,10 @@ impl MyApp {
         // Check the type of the enum then does the appropriate thing
         let Ok(info) = self.btrx.try_recv() else { return; };
         match info {
-            BthrSignal::DiscoveredPeripherals(peris) => self.enumerate_peris(peris),
+            BthrSignal::DiscoveredPeripherals(peris) => self.peris = peris,
             BthrSignal::HeartRate { heart_rate } => self.update_live_heart_rate(heart_rate),
             BthrSignal::StartScan => println!("Started scan..."),
         }
-    }
-
-    fn enumerate_peris(&mut self, peris: Vec<String>) {
-        for per in peris.iter() {
-            println!("from egui thread -> name: {per}");
-        }
-        self.peris = peris;
     }
 
     fn update_live_heart_rate(&mut self, heart_rate: u8) {
@@ -94,7 +87,7 @@ impl eframe::App for MyApp {
 
         let live_hr_label = Label::new(live_hr_text);
 
-        let devices = self.peris
+        let devices_labels = self.peris
             .iter()
             .map(|d_str| Label::new(d_str.as_str()))
             .collect::<Vec<Label>>();
@@ -102,8 +95,27 @@ impl eframe::App for MyApp {
         let central_panel = egui::CentralPanel::default();
         central_panel.show(ctx, |ui| {
             ui.add(live_hr_label);
-            for device in devices {
-                ui.add(device);
+
+            // devices
+            for devices_label in devices_labels {
+                let device_rich_txt = RichText::new(devices_label.text())
+                    .color(Color32::WHITE)
+                    .size(20.0);
+
+                let device_button = Button::new(device_rich_txt)
+                    .fill(Color32::BLUE)
+                    .rounding(Rounding::same(8.0))
+                    .selected(false);
+
+                let device_clicked = ui.add(device_button)
+                    .on_hover_text(format!("test: {}", {devices_label.text()}))
+                    .clicked();
+
+                if device_clicked {
+                    println!("You clicked: {}", devices_label.text());
+                    // try to connect
+                }
+
             }
 
         });
